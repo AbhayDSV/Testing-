b87d75105b7008638927ccaceb2f2b76836d53cf
AbhayDSV <abhay@dsvcorp.com.au>
2026-03-18 15:05:03 +0530

    Fix docker-compose command syntax in deployment workflow and update greeting message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT ?? 8080;
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from abhay test121');
+  res.send('Hello World! from abhay test1211');
 });
 
 app.listen(PORT, () => {
