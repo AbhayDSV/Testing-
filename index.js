de1322340a04bd21853b3aad33d11aa910b7fc05
AbhayDSV <abhay@dsvcorp.com.au>
2026-03-18 15:16:01 +0530

    Fix typo in greeting message in root route response

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT ?? 8080;
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from abhay ankit madarchod');
+  res.send('Hello World! from abhay ankit madarchodd');
 });
 
 app.listen(PORT, () => {
